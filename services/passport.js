ed774c8b9a4ce9b20c9e00725e048bac03a9e5b2
Alexey Ostapiuk <alexostapiuk00@gmail.com>
2018-07-19 08:45:11 +0300

    commit

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,14 @@ passport.use(
     },
     async (email, password, cb) => {
       try {
-        const user = await User.findOne();
+        const user = await User.findOne({ email });
+        if (!user) {
+          return cb(null, false, { error: "No user with this login" });
+        }
+        if (!user.compareSync(password)) {
+          return cb(null, false, { error: "Wrong password" });
+        }
+        cb(null, user);
       } catch (e) {
         return cb(e);
       }
